655350e87b3babdecc26c7219d2d79fa1a9a5c1c
jadecarpenter <151727018+jadecarpenter@users.noreply.github.com>
2023-12-08 13:56:54 +0100

    Update QTclass.py

--- QTclass.py
+++ QTclass.py
@@ -17,12 +17,19 @@ class VehiculeItem(QGraphicsPolygonItem):
     def __init__(self,vehicule):
 
         self.drone = vehicule
-        self.polygone = QPolygon(QPoint(vehicule.posit[0],vehicule.posit[1]), QPoint(vehicule.posit[0] - 1, vehicule.posit[1] - 1), QPoint(vehicule.posit[0] - 1, vehicule.posit[1] + 1))
+        self.polygone = self.polygone = QPolygonF([
+                        QPointF(vehicule.posit[0], vehicule.posit[1]),
+                        QPointF(vehicule.posit[0] - 1, vehicule.posit[1] - 1),
+                        QPointF(vehicule.posit[0] - 1, vehicule.posit[1] + 1),
+                        QPointF(vehicule.posit[0], vehicule.posit[1] + 1)
+                    ])
+        
         super(QGraphicsPolygonItem,self).__init__(self.polygone)
-        self.setRotation(self.drone.orientation[1])
-
+        
+        self.setRotation(self.drone.orient)
         self.setBrush(QBrush(Qt.cyan))
         self.setPen(QPen(Qt.black))
+
 
         def mouseMoveEvent(evt):
             #quand on bouge alors on change la position du drone
@@ -37,33 +44,33 @@ class VehiculeItem(QGraphicsPolygonItem):
 
 
 
-class Q_graphical_item(cm.Drone,cm.Building):
+# class Q_graphical_item(cm.Drone,cm.Building):
 
-    def __init__(self,Lbuild,Lvehic):
-        self.Lbuild = Lbuild
-        self.Lvehic = Lvehic
+#     def __init__(self,Lbuild,Lvehic):
+#         self.Lbuild = Lbuild
+#         self.Lvehic = Lvehic
 
   
-    def dessinerCarre(self):
-        # Dessiner juste un carré sur la scène
-        carre_item = self.scene.addRect(0, 0, 50, 50)
-        carre_item.setPos(50, 50)
-        carre_item.setBrush(QBrush(Qt.red))
-        carre_item.setPen(QPen(Qt.black))
+#     def dessinerCarre(self):
+#         # Dessiner juste un carré sur la scène
+#         carre_item = self.scene.addRect(0, 0, 50, 50)
+#         carre_item.setPos(50, 50)
+#         carre_item.setBrush(QBrush(Qt.red))
+#         carre_item.setPen(QPen(Qt.black))
         
 
-    def ajouterTriangle(self):
-        # Ajouter un triangle à la scène
-        # Utiliser QPolygonF avec QPointF pour définir les points du triangle
-        for v in self.Lvehic:
-            triangle_item = self.scene.addPolygon(QPolygonF([QPointF(cm.v[0].posit[0],cm.v[0].posit[1]), QPointF(cm.v[1].posit[0],cm.v[1].posit[1]), QPointF(cm.v[2].posit[0],cm.v[2].posit[1])]))
-            triangle_item.setPos(150, 50)
-            triangle_item.setBrush(QBrush(Qt.cyan))
-            triangle_item.setPen(QPen(Qt.black))
+#     def ajouterTriangle(self):
+#         # Ajouter un triangle à la scène
+#         # Utiliser QPolygonF avec QPointF pour définir les points du triangle
+#         for v in self.Lvehic:
+#             triangle_item = self.scene.addPolygon(QPolygonF([QPointF(cm.v[0].posit[0],cm.v[0].posit[1]), QPointF(cm.v[1].posit[0],cm.v[1].posit[1]), QPointF(cm.v[2].posit[0],cm.v[2].posit[1])]))
+#             triangle_item.setPos(150, 50)
+#             triangle_item.setBrush(QBrush(Qt.cyan))
+#             triangle_item.setPen(QPen(Qt.black))
         
 
-        # Définir l'angle de rotation pour le triangle (en degrés)
-        triangle_item.setRotation(ang_drone) # Vous pouvez ajuster l'angle selon vos besoins
+#         # Définir l'angle de rotation pour le triangle (en degrés)
+#         triangle_item.setRotation(ang_drone) # Vous pouvez ajuster l'angle selon vos besoins
 
 
 
