46f0c044943f15636712e594dbedb832f50f69e2
loublanchard2 <151726793+loublanchard2@users.noreply.github.com>
2023-12-08 15:14:28 +0100

    Update classmodel.py
    ajout json

--- classmodel.py
+++ classmodel.py
@@ -1,9 +1,9 @@
+import json
+
 class Building:
     def __init__(self,name,verticies):
         self.name = name
         self.verticies = verticies
-
-
 
 class Drone:
     def __init__(self, name, posit, target, orient):
@@ -24,3 +24,23 @@ class Modele:
 
     def add_drone(self,drone):
         self.drones.append(drone)
+
+
+building1 = (1, 3)
+dico_bat={
+        "name": building1[0],
+        "verticies" :building1[1]
+}
+
+drone1=(1,3,2,1)
+dico_drone={
+            "name":drone1[0],
+            "position":drone1[1],
+            "target":drone1[2],
+            "orientation":drone1[3]
+}
+
+with open('data.json', 'w') as mon_fichier:
+	json.dump(dico_bat, mon_fichier)
+	json.dump(dico_drone, mon_fichier)
+
