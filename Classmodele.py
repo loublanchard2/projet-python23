1530d5a072c599ae21352102389987302a00adf7
babaenac <152618805+babaenac@users.noreply.github.com>
2023-12-08 10:29:02 +0100

    Create Classmodele.py

---
+++ Classmodele.py
@@ -0,0 +1,27 @@
+class Building:
+    def __init__(self,name,verticies):
+        self.id = name
+        self.verticies = verticies
+
+
+
+class Vehicule:
+    def __init__(self, name, posit, target, orient):
+        self.id = name
+        self.posit = posit
+        self.target = target
+        self.orient = orient
+
+
+
+class Modele:
+    def __init__(self):
+        self.builings=[]
+        self.vehicules=[]
+
+    def add_building(self,build):
+        self.buildings.append(build)
+
+    def add_vehicule(self,vehic):
+        self.vehicules.append(vehic)
+
